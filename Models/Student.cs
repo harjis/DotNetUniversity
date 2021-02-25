c66c91f82c1e7d533ec2d104072218a4216a24d4
Joonas Harjukallio <harjukallio@gmail.com>
2021-02-25 12:40:17 +0200

    Add max length

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetUniversity.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        
+        [StringLength(50)]
         public string LastName { get; set; }
+        
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+        
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "0:yyyy-MMM-dd", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
         
         public ICollection<Enrollment> Enrollments { get; set; }
