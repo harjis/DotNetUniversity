5f73046376c21eb91dbb85a77895475510c8f1e0
Joonas Harjukallio <harjukallio@gmail.com>
2021-02-26 14:45:11 +0200

    Changes to instructors create

--- Controllers/InstructorsController.cs
+++ Controllers/InstructorsController.cs
@@ -78,6 +78,9 @@ namespace DotNetUniversity.Controllers
         // GET: Instructors/Create
         public IActionResult Create()
         {
+            var instructor = new Instructor();
+            instructor.CourseAssignments = new List<CourseAssignment>();
+            PopulateAssignedCourseData(instructor);
             return View();
         }
 
@@ -86,15 +89,33 @@ namespace DotNetUniversity.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,LastName,FirstMidName,HireDate")]
-            Instructor instructor)
+        public async Task<IActionResult> Create([Bind("LastName,FirstMidName,HireDate, OfficeAssignment")]
+            Instructor instructor,
+            string[] selectedCourses
+        )
         {
+            if (selectedCourses != null)
+            {
+                instructor.CourseAssignments = new List<CourseAssignment>();
+                foreach (var course in selectedCourses)
+                {
+                    var courseToAdd = new CourseAssignment()
+                    {
+                        InstructorId = instructor.Id,
+                        CourseId = int.Parse(course)
+                    };
+                    instructor.CourseAssignments.Add(courseToAdd);
+                }
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Add(instructor);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
+            PopulateAssignedCourseData(instructor);
 
             return View(instructor);
         }
@@ -196,8 +217,17 @@ namespace DotNetUniversity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var instructor = await _context.Instructors.FindAsync(id);
+            var instructor = await _context.Instructors
+                .Include(i => i.CourseAssignments)
+                .SingleAsync(i => i.Id == id);
+            var departments = await _context.Departments
+                .Where(d => d.InstructorId == id)
+                .ToListAsync();
+
+            departments.ForEach(d => d.InstructorId = null);
+
             _context.Instructors.Remove(instructor);
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
