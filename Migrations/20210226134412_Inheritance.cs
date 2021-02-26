2bb53d1c3302f626d520cde87ae83cdab6a06a28
Joonas Harjukallio <harjukallio@gmail.com>
2021-02-26 15:45:36 +0200

    Start with inheritance

---
+++ Migrations/20210226134412_Inheritance.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetUniversity.Migrations
+{
+    public partial class Inheritance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Instructor");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Instructor");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Instructor",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instructor_Person_Id",
+                table: "Instructor",
+                column: "Id",
+                principalTable: "Person",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Person_Id",
+                table: "Student",
+                column: "Id",
+                principalTable: "Person",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instructor_Person_Id",
+                table: "Instructor");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Person_Id",
+                table: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Student",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Student",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Instructor",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Instructor",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Instructor",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -6,25 +6,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DotNetUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int Id { get; set; }
-        
-        [Required, StringLength(50, MinimumLength = 2)]
-        public string LastName { get; set; }
-        
-        [Required, StringLength(50, MinimumLength = 2), Column("FirstName"), Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
-        
         [
             DataType(DataType.Date),
             DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true),
             Display(Name = "Enrollment Date")
         ]
         public DateTime EnrollmentDate { get; set; }
-        
-        public ICollection<Enrollment> Enrollments { get; set; }
 
-        public string FullName => $"{LastName} {FirstMidName}";
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
